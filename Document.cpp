d9e05ca84c5ff5338318d74cf02c88f9e8e48551
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-05-28 16:43:52 +0930

    Update Document.cpp

--- Document.cpp
+++ Document.cpp
@@ -61,8 +61,8 @@ bool Document::Return()
     {
         copies_left = license_limit;
         // made change here 
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
 
